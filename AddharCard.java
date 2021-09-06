24e5af55e60de9f503f64e25580f40bf601ad5f2
Bhoomika-Nadig <89984638+Bhoomika-Nadig@users.noreply.github.com>
2021-09-06 20:41:59 +0530

    Add files via upload

---
+++ AddharCard.java
@@ -0,0 +1,21 @@
+package com.x_workz.controlStatements;
+
+public class AddharCard {
+
+	public static void main(String[] args) {
+		
+		 String Nationality="American";
+			
+			if(Nationality=="Indian") {
+				
+				System.out.println("Addhar card will be generated because u are Indian");
+			}
+				else
+				{
+					System.out.println("Addhar card will not generated because u are not an Indian");
+				}
+			
+				}
+}
+
+
