f13fa7e926def55d4bb78db0e72cd88004fba28d
Bhoomika-Nadig <89984638+Bhoomika-Nadig@users.noreply.github.com>
2021-09-06 21:00:07 +0530

    Add files via upload

---
+++ VotingAge.java
@@ -0,0 +1,19 @@
+package com.x_workz.controlStatements;
+
+public class VotingAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte age = 18;
+		
+	      if(age>=19) {
+	    	  System.out.println(" candidate is eligible for voting");	
+	      }
+	      else
+	      {
+	    	 System.out.println("  candidate is not eligible for voting"); 
+	      }
+	}
+
+}
+
