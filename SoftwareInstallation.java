152bbdd1c7fd723bc1f61746b1116ff7d9edc63a
Bhoomika-Nadig <89984638+Bhoomika-Nadig@users.noreply.github.com>
2021-09-06 20:42:21 +0530

    Add files via upload

---
+++ SoftwareInstallation.java
@@ -0,0 +1,24 @@
+package com.x_workz.controlStatements;
+
+public class SoftwareInstallation {
+	
+
+	public static void main(String[] args) {
+		 
+		 byte ram=6;
+			
+			if(ram>=5) {
+				System.out.println("pubg can be Installed");
+			}
+				else
+				{
+					System.out.println("pubg cannot be Installed");
+				}
+			
+				}
+	
+	}
+
+
+	
+
