29063b651763d4dddc62bfdb5e6661e299259e55
Bhoomika-Nadig <89984638+Bhoomika-Nadig@users.noreply.github.com>
2021-09-06 21:00:27 +0530

    Add files via upload

---
+++ TicketBooking.java
@@ -0,0 +1,15 @@
+package com.x_workz.controlStatements;
+
+public class TicketBooking {
+
+	public static void main(String[] args) {
+
+		      short availability = 1;
+		      
+		      if(availability>=1) {
+		    	  
+		    	  System.out.println("   you can able to book your tickets");	
+		      }
+			}
+
+		}
