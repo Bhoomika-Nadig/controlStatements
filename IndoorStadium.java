4c072fbdf21b688f85c680db0b1773dcf5ded7aa
Bhoomika-Nadig <89984638+Bhoomika-Nadig@users.noreply.github.com>
2021-09-06 20:53:28 +0530

    Add files via upload

---
+++ IndoorStadium.java
@@ -0,0 +1,17 @@
+package com.x_workz.controlStatements;
+
+public class IndoorStadium {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char  gender = 'F';
+		
+		if (gender =='F' || gender =='f'){
+			System.out.println("Female are allowed to enter the stadium");
+		} else {
+			System.out.println("Males are not allowed to enter to stadium");
+		}
+		}
+
+}
